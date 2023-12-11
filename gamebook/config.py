c5b21b1e0e85571a64369e6dbfb736a8be1f2455
Markkelvin14 <122996163+Markkelvin14@users.noreply.github.com>
2023-12-11 02:53:15 -0800

    Update config.py

--- gamebook/config.py
+++ gamebook/config.py
@@ -1,8 +1,11 @@
 import os
+from sqlalchemy import create_engine
 
 class Config:
 	SECRET_KEY = os.environ.get('SECRET_KEY')
+	engine = create_engine('postgresql+psycopg2://postgres:mark@localhost/gamebookdbs')
 	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MAIL_SERVER = 'smtp.googlemail.com'
 	MAIL_PORT = 587
 	MAIL_USE_TLS = True
